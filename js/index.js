91c0b6dd84ddf01394c41b6cc3d9c78ca64af055
Igor <Kankovigor@gmail.com>
2019-12-23 20:05:28 +0000

    all done

---
+++ js/index.js
@@ -0,0 +1,64 @@
+// const age = +prompt("enter your age");
+// if (age < 18 || age > 60){
+//     alert("Don't try this drugs")
+// } else {
+//     alert("You can take it")
+// }
+
+// const name = "Nazar";
+// const surname = "D"
+// if(name && surname){
+//     console.log("yeap")
+// }
+
+// const age = 25;
+// const gender = "male";
+// if (age > 18 && gender === "female") {
+//     console.log("nope")
+// }
+
+// const school = +prompt("Enter school number");
+// const city = prompt("Enter your city");
+// const year = +prompt("Enter finish year");
+// if (school === 193 && city === "Minsk" && year === 1997) {
+//     alert("Great you are my classmate")
+// } else {
+//     alert("You are not my friend")
+// }
+
+// const age = 15;
+// const city = "Ternopile";
+// const hasChildren = false;
+// const isMaried = false;
+// if (age > 12 && age < 17 && city === "Lviv" && !hasChildren && !isMaried){
+//     console.log("Acсess")
+// } else {
+//     console.log("Canceled")
+// }
+
+// const email = "admin"
+// const emailSign = email.indexOf("@") === -1;
+// if (!emailSign && email[0] === "A" ){
+//     alert("Correct email")
+// } else {
+//     alert("no Correct email")
+// }
+
+// let a = 0;
+// while (a < 10){
+//     console.log(a);
+//     a++;
+// }
+
+// const str = "Hello World";
+// let i = 0;
+// while (i < str.length) {
+//     console.log(str[i]);
+//     i++;
+// }
+
+let a = 0;
+do {
+    console.log("hello");
+    a++
+} while(a < 10)
